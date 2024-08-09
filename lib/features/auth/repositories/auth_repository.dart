71caa96299f4e30b123776bfa69c10fe9a9055de
TanishaMehta17 <121849870+TanishaMehta17@users.noreply.github.com>
2024-08-09 16:25:23 +0530

    changes

--- lib/features/auth/repositories/auth_repository.dart
+++ lib/features/auth/repositories/auth_repository.dart
@@ -10,8 +10,12 @@ import 'package:whatsapp_ui/features/auth/screens/user_information_screen.dart';
 import 'package:whatsapp_ui/models/user_model.dart';
 import 'package:whatsapp_ui/screens/mobile_layout_screen.dart';
 
-final authRepositoryProvider = Provider((ref) => AuthRepository(
-    auth: FirebaseAuth.instance, firestore: FirebaseFirestore.instance));
+final authRepositoryProvider = Provider(
+  (ref) => AuthRepository(
+    auth: FirebaseAuth.instance,
+    firestore: FirebaseFirestore.instance,
+  ),
+);
 
 class AuthRepository {
   final FirebaseAuth auth;
@@ -24,6 +28,7 @@ class AuthRepository {
   Future<UserModel?> getCurrentUserData() async {
     var userData =
         await firestore.collection('users').doc(auth.currentUser?.uid).get();
+
     UserModel? user;
     if (userData.data() != null) {
       user = UserModel.fromMap(userData.data()!);
@@ -31,21 +36,25 @@ class AuthRepository {
     return user;
   }
 
-  void signInWithPhone(BuildContext context, phoneNumber) async {
+  void signInWithPhone(BuildContext context, String phoneNumber) async {
     try {
       await auth.verifyPhoneNumber(
-          phoneNumber: phoneNumber,
-          verificationCompleted: (PhoneAuthCredential credential) async {
-            await auth.signInWithCredential(credential);
-          },
-          verificationFailed: (e) {
-            throw Exception(e.message);
-          },
-          codeSent: ((String verificationId, int? resendToken) async {
-            Navigator.pushNamed(context, OTPScreen.routeName,
-                arguments: verificationId);
-          }),
-          codeAutoRetrievalTimeout: (String verificationId) {});
+        phoneNumber: phoneNumber,
+        verificationCompleted: (PhoneAuthCredential credential) async {
+          await auth.signInWithCredential(credential);
+        },
+        verificationFailed: (e) {
+          throw Exception(e.message);
+        },
+        codeSent: ((String verificationId, int? resendToken) async {
+          Navigator.pushNamed(
+            context,
+            OTPScreen.routeName,
+            arguments: verificationId,
+          );
+        }),
+        codeAutoRetrievalTimeout: (String verificationId) {},
+      );
     } on FirebaseAuthException catch (e) {
       showSnackBar(context: context, content: e.message!);
     }
@@ -58,53 +67,74 @@ class AuthRepository {
   }) async {
     try {
       PhoneAuthCredential credential = PhoneAuthProvider.credential(
-          verificationId: verificationId, smsCode: userOTP);
+        verificationId: verificationId,
+        smsCode: userOTP,
+      );
       await auth.signInWithCredential(credential);
       Navigator.pushNamedAndRemoveUntil(
-          context, UserInfromationScreen.routeName, (route) => false);
+        context,
+        UserInfromationScreen.routeName,
+        (route) => false,
+      );
     } on FirebaseAuthException catch (e) {
-      showSnackBar(context: context, content: e.message);
+      showSnackBar(context: context, content: e.message!);
     }
   }
 
-  void saveUserDataToFirebase(
-      {required String name,
-      required File? profilePic,
-      required ProviderRef ref,
-      required BuildContext context}) async {
+  void saveUserDataToFirebase({
+    required String name,
+    required File? profilePic,
+    required ProviderRef ref,
+    required BuildContext context,
+  }) async {
     try {
       String uid = auth.currentUser!.uid;
       String photoUrl =
-          "https://th.bing.com/th/id/OIP.Ghae4OEdb4UmC3hkqpFvLAHaGd?rs=1&pid=ImgDetMain";
+          'https://png.pngitem.com/pimgs/s/649-6490124_katie-notopoulos-katienotopoulos-i-write-about-tech-round.png';
 
       if (profilePic != null) {
         photoUrl = await ref
             .read(commonFirebaseStorageRepositoryProvider)
-            .storeFileToFirebase('profilePic/$uid', profilePic);
+            .storeFileToFirebase(
+              'profilePic/$uid',
+              profilePic,
+            );
       }
+
       var user = UserModel(
-          name: name,
-          uid: uid,
-          profilePic: photoUrl,
-          isOnline: true,
-          phoneNumber: auth.currentUser!.phoneNumber!,
-          groupId: []);
+        name: name,
+        uid: uid,
+        profilePic: photoUrl,
+        isOnline: true,
+        phoneNumber: auth.currentUser!.phoneNumber!,
+        groupId: [],
+      );
 
       await firestore.collection('users').doc(uid).set(user.toMap());
+
       Navigator.pushAndRemoveUntil(
-          context,
-          MaterialPageRoute(builder: (context) => const MobileLayoutScreen()),
-          (route) => false);
+        context,
+        MaterialPageRoute(
+          builder: (context) => const MobileLayoutScreen(),
+        ),
+        (route) => false,
+      );
     } catch (e) {
       showSnackBar(context: context, content: e.toString());
     }
   }
 
- Stream<UserModel> userData(String userId) {
-    return firestore
-        .collection('users')
-        .doc(userId)
-        .snapshots()
-        .map((event) => UserModel.fromMap(event.data()!));
+  Stream<UserModel> userData(String userId) {
+    return firestore.collection('users').doc(userId).snapshots().map(
+          (event) => UserModel.fromMap(
+            event.data()!,
+          ),
+        );
+  }
+
+  void setUserState(bool isOnline) async {
+    await firestore.collection('users').doc(auth.currentUser!.uid).update({
+      'isOnline': isOnline,
+    });
   }
 }
